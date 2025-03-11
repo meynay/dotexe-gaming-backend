f52465ab3a27e2bd4b8d64dc23248c6c6f811707
meynay <mahmoudpanahice@gmail.com>
2025-03-11 13:09:08 +0330

    makes add product and category multipartform

--- internal/delivery/admin_delivery/category.go
+++ internal/delivery/admin_delivery/category.go
@@ -1,21 +1,41 @@
 package admin_delivery
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"store/internal/entities"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (ad *AdminDelivery) AddCategory(c *gin.Context) {
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "unable to parse form"})
+		return
+	}
 	category := entities.Category{}
 	if err := c.BindJSON(&category); err != nil {
 		c.Error(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "bad json format"})
 		return
 	}
-	err := ad.adminusecase.AddCategory(category)
+	primaryImage := form.File["image"]
+	dir := os.Getenv("CATEGORY_DIR")
+	dst := dir + time.Now().String() + primaryImage[0].Filename
+	if err = c.SaveUploadedFile(primaryImage[0], dst); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "couldn't save file"})
+		return
+	}
+	host := os.Getenv("IMAGE_HOST")
+	link := fmt.Sprintf("%s%s", host, dst)
+	log.Printf("File %s uploaded successfully. Link: %s\n", primaryImage[0].Filename, link)
+	category.Image = link
+	err = ad.adminusecase.AddCategory(category)
 	if err != nil {
 		c.Error(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
--- internal/delivery/admin_delivery/product.go
+++ internal/delivery/admin_delivery/product.go
@@ -1,20 +1,53 @@
 package admin_delivery
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"store/internal/entities"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (ad *AdminDelivery) AddProduct(c *gin.Context) {
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "unable to parse form"})
+		return
+	}
 	product := entities.Product{}
 	if c.BindJSON(&product) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "bad json format"})
 		return
 	}
-	err := ad.adminusecase.AddProduct(product)
+	primaryImage := form.File["image"]
+	secondaryImages := form.File["images"]
+	dir := os.Getenv("PRODUCT_DIR")
+	dst := dir + time.Now().String() + primaryImage[0].Filename
+	if err = c.SaveUploadedFile(primaryImage[0], dst); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "couldn't save file"})
+		return
+	}
+	host := os.Getenv("IMAGE_HOST")
+	link := fmt.Sprintf("%s%s", host, dst)
+	log.Printf("File %s uploaded successfully. Link: %s\n", primaryImage[0].Filename, link)
+	product.Image = link
+	images := []string{}
+	for i, file := range secondaryImages {
+		dst = dir + time.Now().String() + string(i) + file.Filename
+		if err = c.SaveUploadedFile(file, dst); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "couldn't save file"})
+			return
+		}
+		link := fmt.Sprintf("%s%s", host, dst)
+		log.Printf("File %s uploaded successfully. Link: %s\n", file.Filename, link)
+		images = append(images, link)
+	}
+	product.Images = images
+	err = ad.adminusecase.AddProduct(product)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
